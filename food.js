0d6c9b14c4b7bbf87e18cb8218d24980ba794fe5
Matheus Vinicius <matheusvinicius7000@gmail.com>
2021-05-27 20:31:58 -0300

    restoring old source code

---
+++ food.js
@@ -0,0 +1,31 @@
+import { onSnake, expandSnake } from './snake.js'
+import { randomGridPosition } from './grid.js'
+
+var eat = new Audio('sound/eat.mp3');
+eat.volume = 0.1;
+
+let food = getRandomFoodPosition()
+const EXPANSION_RATE = 1
+
+export function update() {
+    if (onSnake(food)) {
+        expandSnake(EXPANSION_RATE)
+        eat.play();
+        food = getRandomFoodPosition()
+    }
+}
+export function draw(gameBoard) {
+    const foodElement = document.createElement('div')
+    foodElement.style.gridRowStart = food.y
+    foodElement.style.gridColumnStart = food.x
+    foodElement.classList.add('food')
+    gameBoard.appendChild(foodElement)
+}
+
+function getRandomFoodPosition() {
+    let newFoodPosition
+    while (newFoodPosition == null || onSnake(newFoodPosition)) {
+        newFoodPosition = randomGridPosition()
+    }
+    return newFoodPosition
+}
---
+++ input.js
@@ -0,0 +1,35 @@
+let inputDirection = { x: 0, y: 0 }
+let lastInputDirection = { x: 0, y: 0 }
+
+var move = new Audio('sound/movement.mp3');
+move.volume = 0.1;
+
+window.addEventListener('keydown', e => {
+    switch (e.key) {
+        case 'ArrowUp':
+            if (lastInputDirection.y !== 0) break
+            inputDirection = { x: 0, y: -1 }
+            move.play();
+            break
+        case 'ArrowDown':
+            if (lastInputDirection.y !== 0) break
+            inputDirection = { x: 0, y: 1 }
+            move.play();
+            break
+        case 'ArrowLeft':
+            if (lastInputDirection.x !== 0) break
+            inputDirection = { x: -1, y: 0 }
+            move.play();
+            break
+        case 'ArrowRight':
+            if (lastInputDirection.x !== 0) break
+            inputDirection = { x: 1, y: 0 }
+            move.play();
+            break
+    }
+})
+
+export function getInputDirection() {
+    lastInputDirection = inputDirection
+    return inputDirection
+}
