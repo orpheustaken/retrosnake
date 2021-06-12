493b14062ada133544f43f94f88c0bad1e954679
Matheus Vinicius <matheusvinicius7000@gmail.com>
2021-06-12 16:54:32 -0300

    console entries removed

--- js/score.js
+++ js/score.js
@@ -5,23 +5,11 @@ let record = ('0' + Number(localStorage.getItem('record'))).slice(-2)
 export function updateScore() {
     score++
     formatScore = ('0' + score).slice(-2)
-    console.clear()
-    console.log("score: " + score)
 }
 
 export function updateRecord() {
-    if (score > record && record != 0) {
-        console.log("you achieved a new record\n\n")
-        console.log("previous record: " + record)
+    if (score > record) {
         newRecord()
-        console.log("new record: " + record)
-    }
-    else if (record != 0) {
-        console.log("\ncurrent record: " + record)
-    }
-    else {
-        newRecord()
-        console.log("\nfirst game? keep playing")
     }
 }
 
