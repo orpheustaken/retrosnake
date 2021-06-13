aad633ebffae09f0078f21d208c8a1a98969c841
Matheus Vinicius <matheusvinicius7000@gmail.com>
2021-06-13 18:40:08 -0300

    wasd and hjkl now can control the snake

--- js/input.js
+++ js/input.js
@@ -8,21 +8,33 @@ move.volume = vol
 
 window.addEventListener('keydown', e => {
     switch (e.key) {
+        case 'k':
+        case 'w':
+        case 'W':
         case 'ArrowUp':
             if (lastInputDirection.y !== 0) break
             inputDirection = { x: 0, y: -1 }
             move.play()
             break
+        case 'j':
+        case 's':
+        case 'S':
         case 'ArrowDown':
             if (lastInputDirection.y !== 0) break
             inputDirection = { x: 0, y: 1 }
             move.play()
             break
+        case 'h':
+        case 'a':
+        case 'A':
         case 'ArrowLeft':
             if (lastInputDirection.x !== 0) break
             inputDirection = { x: -1, y: 0 }
             move.play()
             break
+        case 'l':
+        case 'd':
+        case 'D':
         case 'ArrowRight':
             if (lastInputDirection.x !== 0) break
             inputDirection = { x: 1, y: 0 }
